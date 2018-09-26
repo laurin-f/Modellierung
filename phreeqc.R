c7e905e848b5a2c65f35f061d3de82ec8ac87eaf
Laurin Freiberg <laurin_f@posteo.de>
2018-09-26 10:03:53 +0200

    gui func

--- phreeqc.R
+++ phreeqc.R
@@ -1,6 +1,16 @@
-phreeqc<-shell(paste('"start "" C:/Program Files/USGS/phreeqc-3.4.0-12927-x64/bin/phreeqci.exe" '))
-
-
-
 phreeqc<-function(input){shell(paste0('cd C:/Users/ThinkPad/Documents/Masterarbeit/daten/PhreeqC/phreeqc-3.4.0-12927-x64/bin && phreeqc C:\\Users\\ThinkPad\\Documents\\Masterarbeit\\daten\\PhreeqC\\',input))}
+
 phreeqc("calcium")
+
+#sendKeys 
+phreeqc_gui<-function(file,path){
+  path<-gsub("/","\\\\\\\\",path)
+script<-readLines("C:/Users/ThinkPad/Documents/Masterarbeit/daten/PhreeqC/psinput",encoding = "utf8")
+writeLines(sub("pfad",paste0(path,file),script),
+           "C:/Users/ThinkPad/Documents/Masterarbeit/daten/PhreeqC/psinput.ps1")
+#shell("powershell.exe -noprofile -executionpolicy bypass -file C:/Users/ThinkPad/Documents/Masterarbeit/daten/PhreeqC/phreeqc.ps1")
+shell("powershell.exe -noprofile -executionpolicy bypass -file C:/Users/ThinkPad/Documents/Masterarbeit/daten/PhreeqC/psinput.ps1")
+print(paste0(path,file))}
+
+phreeqc_gui("calcium.pqi","C:/Users/ThinkPad/Documents/Masterarbeit/daten/PhreeqC/")
+
