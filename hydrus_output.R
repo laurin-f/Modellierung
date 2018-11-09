e9327f2926e34b1fbd727db55883c408360c2f38
Laurin Freiberg <laurin_f@posteo.de>
2018-11-09 17:43:00 +0100

    hydrus

--- hydrus_output.R
+++ hydrus_output.R
@@ -1,35 +1,25 @@
 hydruspfad<-"C:/Users/ThinkPad/Documents/Masterarbeit/daten/hydrus/"
-projektpfad<-"C:/Users/ThinkPad/Documents/Masterarbeit/daten/hydrus/undisturbed/"
+projektpfad1<-"C:/Users/ThinkPad/Documents/Masterarbeit/daten/hydrus/undisturbed/"
+projektpfad2<-"C:/Users/ThinkPad/Documents/Masterarbeit/daten/hydrus/undisturbed2/"
 programmpfad<-"C:/Users/ThinkPad/Documents/Masterarbeit/programme/Hydrus-1D 4.xx/"
 
-hydrus_gui<-function(file,path=hydruspfad){
-  path<-gsub("/","\\\\\\\\",path)
-  
-  scriptpath<-"C:/Users/ThinkPad/Documents/Masterarbeit/daten/hydrus/"
-  
-  script<-readLines(paste0(scriptpath,"hydrus_psinput.txt"))
-  
-  writeLines(sub("pfad",paste0(path,file),script),
-             paste0(scriptpath,"psinput.ps1"))
-  
-  shell(paste0("powershell.exe -noprofile -executionpolicy bypass -file ",scriptpath,"psinput.ps1"))}
 
-
-hydrus.exe<-function(file="undisturbed",scriptpath="C:/Users/ThinkPad/Documents/Masterarbeit/daten/hydrus/",sleep=3){
+hydrus.exe<-function(file="undisturbed",scriptpath="C:/Users/ThinkPad/Documents/Masterarbeit/daten/hydrus/",sleep=3,UNSC=T){
   level_01<-readLines(paste0(hydruspfad,"Level_01.dir"))
   level_01<-sub("file",file,level_01)
   writeLines(level_01,paste0(programmpfad,"Level_01.dir"))
   
   script<-readLines(paste0(scriptpath,"hydrus_exe2.txt"))
-  
-  writeLines(sub("secs",sleep,script),
-             paste0(scriptpath,"hydrus_exe2.ps1"))
+  script<-sub("secs",sleep,script)
+  if(UNSC==F){
+  script<-sub("UNSC","CALC",script)}
+  writeLines(script,paste0(scriptpath,"hydrus_exe2.ps1"))
   shell(paste0("powershell.exe -noprofile -executionpolicy bypass -file ",scriptpath,"hydrus_exe2.ps1"))}
 
 
 load("C:/Users/ThinkPad/Documents/Masterarbeit/daten/all.R")
 
-atmos.in<-function(int,event,total_t,projektpfad=projektpfad){
+atmos.in<-function(int,event,total_t,projektpfad=projektpfad1){
 lines<-readLines(paste0(hydruspfad,"ATMOSPHtemp.IN"))
 lines<-sub("event",event,lines)
 lines<-sub("stop",event+5,lines)
@@ -46,7 +36,7 @@ selector.in<-function(thr,
                       ks,
                       l,
                       n_mats=3,
-                      projektpfad=projektpfad){
+                      projektpfad=projektpfad1){
   lines<-readLines(paste0(projektpfad,"SELECTOR.IN"))
   vals<-paste(thr,ths,alfa,n,ks,l)
   soil_param_pos<-(grep("thr",lines)+1):(grep("thr",lines)+n_mats)
@@ -57,7 +47,7 @@ profile.in<-function(th=seq(0.1,0.2,len=10),
                      Mat=1,
                      Temp=20,
                      Conc=0.0004,
-                     projektpfad=projektpfad){
+                     projektpfad=projektpfad1){
   lines<-readLines(paste0(projektpfad,"PROFILE.DAT"))
   n_nodes<-length(lines)-7
   #vals<-paste(1:n_nodes,-(1:n_nodes)+1,th,Mat,"1  0.000000e+000    1    1    1",Temp,Conc)
@@ -73,17 +63,25 @@ profile.in<-function(th=seq(0.1,0.2,len=10),
 
 
 
-read_hydrus.out<-function(treat=17,projektpfad=projektpfad){lines_node<-readLines(paste0(projektpfad,"Obs_Node.out"))
+read_hydrus.out<-function(treat=17,projektpfad=projektpfad1,UNSC=T){lines_node<-readLines(paste0(projektpfad,"Obs_Node.out"))
 obs_node<-read.table(paste0(projektpfad,"Obs_Node.out"),skip=10,nrows = length(lines_node)-12,header = T)
 tiefenstufen<-c(-2,-6,-10,-14)
 colnames(obs_node)<-c("t_min",rep(tiefenstufen,each=4))
-CO2_node<-obs_node[,c(1,5,9,13,17)]
+
+
 theta_node<-obs_node[,c(1,3,7,11,15)]
-CO2_mod<-data.table::melt(CO2_node,id=1)
 theta_mod<-data.table::melt(theta_node,id=1)
 
+if(UNSC==T){
+CO2_node<-obs_node[,c(1,5,9,13,17)]
+CO2_mod<-data.table::melt(CO2_node,id=1)
 vals_mod<-merge(CO2_mod,theta_mod,by=c("t_min","variable"))
 colnames(vals_mod)<-c("t_min","tiefe","CO2_mod","theta_mod")
+}else{
+  vals_mod<-theta_mod
+  colnames(vals_mod)<-c("t_min","tiefe","theta_mod")
+  }
+
 max(vals_mod$t_min)
 t_min<-1:max(vals_mod$t_min)
 mod_list<-vector("list",4)
@@ -98,11 +96,17 @@ sub<-subset(all,treatment==treat&tiefe%in%tiefenstufen)
 sub<-merge(sub,modelled_approx,by=c("t_min","tiefe"),all="T")
 
 rmse_theta<-sqrt(mean((sub$theta-sub$theta_mod)^2,na.rm = T))
+if(UNSC==T){
 rmse_CO2<-sqrt(mean((sub$CO2_raw-sub$CO2_mod)^2,na.rm = T))
-return(list(sub,c(rmse_theta,rmse_CO2)))}
+rmse<-c(rmse_theta,rmse_CO2)
+}else{
+  rmse<-rmse_theta
+}
+return(list(sub,rmse))}
 
 
 library(ggplot2)
+
 selector.in(thr=0.068,
             ths=0.61,
             alfa=0.075,
@@ -114,8 +118,26 @@ profile.in()
 hydrus.exe(sleep = 8)
 sub<-read_hydrus.out(treat=17)[[1]]
 
+ggplot(sub)+geom_point(aes(t_min,theta,col="obs"),na.rm = T)+geom_line(aes(t_min,theta_mod,col="mod"),na.rm = T)+facet_wrap(~tiefe,ncol=1)
+
+
+
+selector.in(thr=0.068,
+            ths=0.61,
+            alfa=0.075,
+            n=1.89,
+            ks=0.0273,
+            l=0.5,
+            projektpfad = projektpfad2)
+atmos.in(0.0026,180,4000,
+         projektpfad = projektpfad2)
+profile.in(projektpfad = projektpfad2)
+hydrus.exe(sleep = 8,file = "undisturbed2",UNSC=F)
+
+sub<-read_hydrus.out(treat=17,projektpfad = projektpfad2,UNSC=F)[[1]]
 
 ggplot(sub)+geom_point(aes(t_min,theta,col="obs"),na.rm = T)+geom_line(aes(t_min,theta_mod,col="mod"),na.rm = T)+facet_wrap(~tiefe,ncol=1)
+
 
 
 ggplot(sub)+geom_point(aes(t_min,CO2_raw,col="obs"),na.rm = T)+geom_line(aes(t_min,CO2_mod*1000000,col="mod"),na.rm = T)+facet_wrap(~tiefe,ncol=1)
